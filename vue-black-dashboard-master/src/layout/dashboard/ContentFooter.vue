adae6c11e546673611718ed0c1d9e1b49399b329
Nacharapol B <68985985+nacharapol@users.noreply.github.com>
2020-10-14 12:12:03 +0700

    141020_1

--- vue-black-dashboard-master/src/layout/dashboard/ContentFooter.vue
+++ vue-black-dashboard-master/src/layout/dashboard/ContentFooter.vue
@@ -2,26 +2,16 @@
   <footer class="footer">
     <div class="container-fluid">
       <ul class="nav">
+        
         <li class="nav-item">
-          <a class="nav-link" href="http://www.creative-tim.com">
-            Creative Tim
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://blog.creative-tim.com">
-            Blog
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://www.creative-tim.com/license">
-            Licenses
+          <a class="nav-link" href="#">
+            ระบบควบคุมประตูน้ำ
           </a>
         </li>
       </ul>
       <div class="copyright">
-        © {{year}} made with <i class="tim-icons icon-heart-2"></i> by
-        <a href="https://www.binarcode.com" target="_blank" rel="noopener">Binar Code</a>&nbsp;&
-        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a> for a better web.
+        © {{year}} developed by
+        <a href="#" target="_blank" rel="noopener">IoT Team</a>
       </div>
     </div>
   </footer>
--- vue-black-dashboard-master/src/layout/dashboard/TopNavbar.vue
+++ vue-black-dashboard-master/src/layout/dashboard/TopNavbar.vue
@@ -13,7 +13,7 @@
             <span class="navbar-toggler-bar bar3"></span>
           </button>
         </div>
-        <a class="navbar-brand" href="#pablo">{{routeName}}</a>
+        <a class="navbar-brand" href="#pablo"><img src="http://www.verifyth.com/IoT_img/sys01_logo_2.png"></a>
       </div>
       <button class="navbar-toggler" type="button"
               @click="toggleMenu"
